4656ff999041fc0bdc79f2f875a4aa97082e8644
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-20 09:29:07 +1000

    Removed copyright

--- peek_plugin_active_task/_private/storage/DeclarativeBase.py
+++ peek_plugin_active_task/_private/storage/DeclarativeBase.py
@@ -1,13 +1,3 @@
-""" 
- *  Copyright Synerty Pty Ltd 2013
- *
- *  This software is proprietary, you are not free to copy
- *  or redistribute this code in any format.
- *
- *  All rights to this software are reserved by 
- *  Synerty Pty Ltd
- *
-"""
 
 from sqlalchemy.ext.declarative import declarative_base
 
