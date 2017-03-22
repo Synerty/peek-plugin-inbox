7f97eb5f1ea74c1fa27fef59407bdb92cbadb41d
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-22 11:14:51 +1000

    Fixed issue with Settings.py trying to use with block on ormSession

--- peek_plugin_active_task/_private/storage/Setting.py
+++ peek_plugin_active_task/_private/storage/Setting.py
@@ -254,8 +254,8 @@ def _getSetting(ormSession, name, propertyDict, key=None, value=None):
     ormSession.commit()
 
     # Close after return
-    with ormSession:
-        return setting[key]
+    return setting[key]
+
 
 
 # =============================================================================
