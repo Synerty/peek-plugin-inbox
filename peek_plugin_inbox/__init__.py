31b0780031f3eb17db33465f0f1f169c9d71474d
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-07-30 16:12:39 +1000

    Updated to version 1.1.0

--- peek_plugin_inbox/__init__.py
+++ peek_plugin_inbox/__init__.py
@@ -3,7 +3,7 @@ from typing import Type
 from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHookABC
 from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHookABC
 
-__version__ = '1.0.0.4'
+__version__ = '1.1.0'
 
 
 def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
